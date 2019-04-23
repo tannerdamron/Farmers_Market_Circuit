05710bcbfe4579d3c6ac3914d012b11a0185f613
Jimmy Zebroski and Tanner Damron <jimmy+tanner@>
2019-04-23 13:58:32 -0700

    add styling and looping

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'React Help Queue',
+      title: "Avery's Organics",
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
